48544256f026a63912cb83b1a26fe6f4f58c7604
hoohoohoohoo <43032545+hoohoohoohoo@users.noreply.github.com>
2018-11-20 16:17:59 +0900

    Add files via upload

---
+++ study1.py
@@ -0,0 +1,13 @@
+str = int (input("얼마를 가지고 계세요?"))
+
+print("%d만큼가지고 있습니다."%str)
+
+print("가나다라마바 \n 사아자차카타파하")
+
+str = input("이름을 입력하시오.")
+
+print("%s"%str*10)
+
+age = int(input("나이를 입력하시오."))
+name = input("이름을 입력하시오.")
+print("%d세이고 이름은 %s입니다."%(age, name))
